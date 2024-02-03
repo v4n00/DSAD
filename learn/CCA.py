fa20d0065a368e8edbb01ad18edd34cff610b63d
v4n00 <105587619+v4n00@users.noreply.github.com>
2024-02-03 20:59:33 +0200

    fixed some typos

--- learn/CCA.py
+++ learn/CCA.py
@@ -7,10 +7,10 @@ y = np.ndarray() # standardized
 p = x.shape[1]
 q = y.shape[1]
 m = min(p, q)
-modelCCA = CCA(n_components=m)
-z, u = modelCCA.fit_transform(x, y)
-Rxz = np.corrcoef(x, z[:, :m], rowvar=False)[:p, p:]
-Ryu = np.corrcoef(y, u[:, :m], rowvar=False)[:q, q:]
+cca = CCA(n_components=m)
+z, u = cca.fit_transform(x, y)
+rxz = np.corrcoef(x, z[:, :m], rowvar=False)[:p, p:]
+ryu = np.corrcoef(y, u[:, :m], rowvar=False)[:q, q:]
 r = []
 for i in range(m):
     r.append(np.corrcoef(z[:, i], u[:, i], rowvar=False)[0, 1])
--- learn/LDA.py
+++ learn/LDA.py
@@ -9,9 +9,9 @@ tinta = 'VULNERAB' # column specified in the requirements
 variabile = list(x.columns.values[:-1]) # the other columns
 
 x_train, x_test, y_train, y_test = train_test_split(x[variabile], x[tinta], train_size=0.4)
-model = LinearDiscriminantAnalysis()
-model.fit(x_train, y_train) # trains the model
+lda = LinearDiscriminantAnalysis()
+lda.fit(x_train, y_train) # trains the model
 
-scores = model.transform(x_test)
-prediction_test = model.predict(x_test)
-prediction_applied = model.predict(x_applied)
+scores = lda.transform(x_test)
+prediction_test = lda.predict(x_test)
+prediction_applied = lda.predict(x_applied)
